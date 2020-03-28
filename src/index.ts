7fb8a70d6e0673280a0f0e24b55a81b65aca38a2
tana-gh <yozoranotakaku@gmail.com>
2020-03-28 17:26:15 +0900

    Fix unperform material icons

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import App     from './components/App.vue'
 import router  from './router'
 import '../assets/scss/style.scss'
 import 'vuetify/dist/vuetify.min.css'
-import 'material-icons/iconfont/material-icons.css'
+import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.use(Router)
 Vue.use(Vuetify)
 
-const app = new Vue({
+new Vue({
     render: h => h(App),
     router,
     vuetify: new Vuetify()
